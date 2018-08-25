4ed46d3e5de355ff545670b19f195af99750f548
jonanpogi <junan.bie@gmail.com>
2018-08-25 21:41:45 +0800

    added local notification

---
+++ src/pages/notifications/notifications.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ViewController, AlertController, Platform } from 'ionic-angular';
+import { LocalNotifications } from '@ionic-native/local-notifications';
+
+/**
+ * Generated class for the NotificationsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-notifications',
+  templateUrl: 'notifications.html',
+})
+export class NotificationsPage {
+
+  data = { title:'', description:'', date:'', time:'' };
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, public alertCtrl: AlertController, public localNotif: LocalNotifications, public platform: Platform) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad NotificationsPage');
+  }
+
+  submit() {
+    console.log(this.data);
+    var date = new Date(this.data.date+" "+this.data.time);
+    console.log(date);
+    this.localNotif.schedule({
+       text: 'Delayed ILocalNotification',
+       trigger : {at: date},
+       led: 'FF0000',
+       sound: this.setSound(),
+    });
+    let alert = this.alertCtrl.create({
+      title: 'Congratulation!',
+      subTitle: 'Notification setup successfully at '+date,
+      buttons: ['OK']
+    });
+    alert.present();
+    this.data = { title:'', description:'', date:'', time:'' };
+  }
+
+  setSound() {
+    if (this.platform.is('android')) {
+      return 'file://assets/sounds/Rooster.mp3'
+    } else {
+      return 'file://assets/sounds/Rooster.caf'
+    }
+  }
+
+  close(){
+    this.viewCtrl.dismiss();
+  }
+
+}
