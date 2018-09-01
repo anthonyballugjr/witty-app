d734305c60f97004952d0210100f4d4b3b43ca76
jonanpogi <junan.bie@gmail.com>
2018-09-01 12:24:52 +0800

    html to pdf

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {TabsPage} from '../tabs/tabs';
+import * as jsPDF from 'jspdf';
 
 /**
  * Generated class for the ProfilePage page.
@@ -16,7 +17,7 @@ import {TabsPage} from '../tabs/tabs';
 })
 export class ProfilePage {
   userData = localStorage
- 
+  @ViewChild('content') content: ElementRef;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     console.log(this.userData);
@@ -24,6 +25,26 @@ export class ProfilePage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
+  }
+
+  export(){
+    
+    let doc = new jsPDF();
+
+    let specialElementHandlers = {
+      '#editor': function(element, renderer){
+        return true;
+      }
+    };
+
+    let content = this.content.nativeElement;
+
+    doc.fromHTML(content.innerHTML, 15,15, {
+      'width': 190,
+      'elementHandlers': specialElementHandlers
+    });
+
+    doc.save('transcript.pdf');
   }
 
 }
