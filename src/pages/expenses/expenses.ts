2b104e6d7439b1a0e44e31a1e8ae1a9e62091ea3
Anthony <anthonyballugjr@gmial.com>
2018-09-10 00:39:14 +0800

    updated view

--- src/pages/expenses/expenses.ts
+++ src/pages/expenses/expenses.ts
@@ -7,14 +7,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'expenses.html',
 })
 export class ExpensesPage {
-  expenses: any = [];
+  expenses: any;
+
+  today = new Date().getDay;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.expenses = this.navParams.get('expenses');
   }
 
   ionViewDidLoad() {
-    
   }
-
+ 
 }
