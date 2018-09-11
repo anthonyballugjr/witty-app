b794aae903e35ff194e53a6c2419ed2d854ac49d
Anthony <anthonyballugjr@gmial.com>
2018-09-12 02:41:26 +0800

    disabled menu swipe

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController, LoadingController, ToastController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController, ToastController, MenuController } from 'ionic-angular';
 import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 import { TabsPage } from '../tabs/tabs';
 import { SignupPage } from '../signup/signup';
@@ -8,6 +8,7 @@ import { HttpClient } from '@angular/common/http';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 import { AuthProvider } from '../../providers/auth/auth'
+import { Menu } from 'ionic-angular/umd/components/app/menu-interface';
 
 @IonicPage()
 @Component({
@@ -29,18 +30,25 @@ export class LoginPage {
   message: any;
   userData: any;
 
-  constructor(private toastCtrl: ToastController, public authProvider: AuthProvider, private formBldr: FormBuilder, public http: HttpClient, private facebook: Facebook, public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public loadingCtrl: LoadingController) {
+  constructor(private toastCtrl: ToastController, public authProvider: AuthProvider, private formBldr: FormBuilder, public http: HttpClient, private facebook: Facebook, public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public loadingCtrl: LoadingController, private menuCtrl: MenuController) {
     this.loginForm = this.formBldr.group({
       email: ["", Validators.required],
       password: ["", Validators.required]
-    }); 
+    });
   }
 
+  ionViewDidEnter() {
+    this.menuCtrl.enable(false);
+    this.menuCtrl.swipeEnable(false);
+  }
 
+  ionViewWillLeave() {
+    this.menuCtrl.enable(true);
+    this.menuCtrl.swipeEnable(true);
+  }
 
   ionViewDidLoad() {
-    //this.showLoader();
-    //check authentication
+    console.log('Welcome to Witty Wallet');
   }
 
   showLoader() {
@@ -54,7 +62,7 @@ export class LoginPage {
     let toast = this.toastCtrl.create({
       message: msg,
       duration: 3000,
-      position: 'bottom',
+      position: 'top',
       dismissOnPageChange: false
     });
     toast.onDidDismiss(() => {
@@ -70,30 +78,37 @@ export class LoginPage {
   login() {
     this.showLoader();
     this.authProvider.login(this.loginData).then((result) => {
+      console.log(result);
       this.loading.dismiss();
       this.navCtrl.setRoot(TabsPage);
       this.presentToast('Hello, ' + localStorage.nickname);
     }, (err) => {
       this.loading.dismiss();
-      if (err.status === 401) {
-        this.errMessage = 'User not found';
-      }
-      this.presentToast(this.errMessage);
+      this.presentToast(err.error);
       console.log(err);
     });
   }
 
-  loginWithFB(userData) {
+  loginWithFB() {
     this.facebook.login(['email', 'public_profile'])
       .then((res: FacebookLoginResponse) => {
         this.facebook.api('me?fields=id,name,email,first_name,picture.width(100).height(100).as(picture)', [])
           .then(profile => {
             this.userDataFB = { email: profile['email'], firstName: profile['first_name'], picture: profile['picture']['data']['url'], username: profile['name'] }
-
+            let fbData = {
+              user: {
+                'email': profile['email']
+              }
+            }
             console.log(this.userDataFB);
-            this.navCtrl.setRoot(TabsPage, { userData: this.userDataFB });
+
+            // this.navCtrl.setRoot(TabsPage, { userData: this.userDataFB });
           });
       });
+  }
+
+  forgotPassword() {
+    console.log('Forgot password');
   }
 
 }
