17dc6642a8745838890d2b2e1c68336adb892a74
LysleB <falalayl@gmail.com>
2018-09-27 17:34:45 +0800

    added (static) UI to saving challenges

--- src/pages/challenges/challenges.ts
+++ src/pages/challenges/challenges.ts
@@ -8,15 +8,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ChallengesPage {
   challengeStatus = false;
-
+  spareChange = false;
+  ftWeek = false;
+  
   challenges = [
     {
+      challengeID: 'spareChange',
       title: 'The Spare Change Challenge',
       description: 'The spare change challenge is simply what the name suggests. Throughout the year, the goal is to save all of your spare change.',
       expectedAmount: 0
 
     },
     {
+      challengeID: 'ftWeek',
       title: '52-Week Challenge',
       description: 'This challenge involves saving an increasing amount each week until the end of the year. This version of the challenge starts at PHP 5 and has a weekly increment of PHP 5 each week.',
       expectedAmount: 6890
@@ -25,15 +29,26 @@ export class ChallengesPage {
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-
+   
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ChallengesPage');
+    console.log('s ' + this.spareChange);
+    console.log('f: ' + this.ftWeek);
   }
 
-  takeChallenge(){
+  takeChallenge(challengeID){
     this.challengeStatus = true;
+    console.log(challengeID);
+    
+    if(challengeID === 'spareChange'){
+      this.spareChange = true;
+    }
+    else if(challengeID === 'ftWeek'){
+      this.ftWeek = true;
+    }
+    
   }
 
 }
