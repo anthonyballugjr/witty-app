7f87ba1ecabea440e4e21117cc8c697ef43f5331
anthonyballugjr <anthonyballugjr@gmail.com>
2018-12-27 20:52:22 +0800

    added withdrawals on savings wallets

--- src/pages/view-deposits/view-deposits.ts
+++ src/pages/view-deposits/view-deposits.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController, AlertController, LoadingController, ToastController } from 'ionic-angular';
 import { DepositsProvider } from '../../providers/deposits/deposits';
+import { TransactionsProvider } from '../../providers/transactions/transactions';
 
 
 @IonicPage()
@@ -11,35 +12,44 @@ import { DepositsProvider } from '../../providers/deposits/deposits';
 export class ViewDepositsPage {
   view: string = "deposits";
 
-  deposits: any;
+
   deposit = {
     amount: '',
     walletId: '',
     period: localStorage.period
   }
+  deposits: any;
+  totalDeposits: any;
 
   withdrawal = {
     desc: 'withdrawal',
     walletId: '',
     amount: ''
   }
+  withdrawals: any;
+  totalWithdrawals: any;
 
   walletId: any;
   walletName: any;
+  wallet: any;
 
   prompt: any;
   loading: any;
   result: any;
   depositData: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController, private alertCtrl: AlertController, private loadCtrl: LoadingController, private toastCtrl: ToastController, public depositsProvider: DepositsProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController, private alertCtrl: AlertController, private loadCtrl: LoadingController, private toastCtrl: ToastController, public depositsProvider: DepositsProvider, public transactionsProvider: TransactionsProvider) {
+    this.wallet = this.navParams.get('wallet');
     this.walletId = this.navParams.get('_id');
     this.walletName = this.navParams.get('walletName');
-    this.getDeposits();
+    this.doAll();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ViewDepositsPage');
+  async doAll() {
+    await this.presentLoad('Fetching wallet data...');
+    await this.getDeposits();
+    await this.getTransactions();
+    await this.loading.dismiss();
   }
 
   presentLoad(content) {
@@ -53,7 +63,7 @@ export class ViewDepositsPage {
     this.loading.present();
   }
 
-  presentToast(msg){
+  presentToast(msg) {
     let toast = this.toastCtrl.create({
       message: msg,
       duration: 2000,
@@ -66,19 +76,40 @@ export class ViewDepositsPage {
   }
 
   getDeposits() {
-    this.presentLoad('Fetching deposits...');
     this.depositsProvider.getDeposits(this.walletId)
       .then(data => {
         this.deposits = data;
+        var a = - 0;
+        for (let x of this.deposits) {
+          a += x.amount;
+        }
+        this.totalDeposits = a;
+        console.log('Total Deposits', this.totalDeposits);
         console.log('Deposits', this.deposits);
-        this.loading.dismiss();
       }, err => {
         this.loading.dismiss();
         console.log(err);
       });
   }
 
-  addDeposit(){
+  getTransactions() {
+    this.transactionsProvider.getTransactions(this.wallet._id)
+      .then(data => {
+        this.withdrawals = data;
+        var a = 0;
+        for (let x of this.withdrawals) {
+          a += x.amount;
+        }
+        this.totalWithdrawals = a;
+        console.log('Total Withdrawals', this.totalWithdrawals);
+        console.log('Withdrawals', this.withdrawals);
+      }, err => {
+        this.loading.dismiss();
+        console.log(err);
+      });
+  }
+
+  addDeposit() {
     let alert = this.alertCtrl.create({
       title: 'New Deposit',
       enableBackdropDismiss: false,
@@ -88,7 +119,7 @@ export class ViewDepositsPage {
           name: 'amount',
           label: 'Amount',
           type: 'number',
-          placeholder: 'Enter amount'
+          placeholder: 'Enter amount to deposit'
         }
       ],
       buttons: [
@@ -98,12 +129,12 @@ export class ViewDepositsPage {
         },
         {
           text: 'Add',
-          handler: (data) =>{
-            if(data.amount === ''){
+          handler: (data) => {
+            if (data.amount === '') {
               this.presentToast('Amount must not be empty');
               return false;
             }
-            else{
+            else {
               this.deposit.walletId = this.walletId;
               this.deposit.period = localStorage.period;
               this.deposit.amount = data.amount;
@@ -112,23 +143,23 @@ export class ViewDepositsPage {
                 buttons: [
                   {
                     text: 'Cancel',
-                    role:'cancel'
+                    role: 'cancel'
                   },
                   {
                     text: 'Agree',
-                    handler: ()=>{
+                    handler: () => {
                       this.presentLoad('Adding deposit...');
                       this.depositsProvider.addDeposit(this.deposit)
-                      .then(result=>{
-                        this.loading.dismiss();
-                        this.presentToast('Deposit added!');
-                        alert.dismiss();
-                        this.getDeposits();
-                      },err=>{
-                        this.loading.dismiss();
-                        console.log(err);
-                        this.presentToast(err.message);
-                      });
+                        .then(result => {
+                          this.loading.dismiss();
+                          this.presentToast('Deposit added!');
+                          alert.dismiss();
+                          this.getDeposits();
+                        }, err => {
+                          this.loading.dismiss();
+                          console.log(err);
+                          this.presentToast(err.message);
+                        });
                     }
                   }
                 ]
@@ -143,7 +174,94 @@ export class ViewDepositsPage {
     alert.present();
   }
 
-  close(){
+  addTransaction() {
+    this.prompt = this.alertCtrl.create({
+      title: 'New Withdrawal',
+      enableBackdropDismiss: false,
+      subTitle: 'Withdraw from this wallet',
+      message: `Remaining Amount: ₱${(this.totalDeposits - this.totalWithdrawals).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}`,
+      inputs: [
+        {
+          label: 'Amount',
+          type: 'number',
+          name: 'amount',
+          placeholder: 'Enter amount to withdraw'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelled');
+          }
+        },
+        {
+          text: 'Add',
+          handler: (data) => {
+            if (data.amount === '') {
+              this.presentToast('Amount must not be empty');
+              return false;
+            }
+            else if ((data.amount > this.totalDeposits) || ((parseFloat(data.amount) + parseFloat(this.totalWithdrawals)) > this.totalDeposits)) {
+              let warning = this.alertCtrl.create({
+                title: 'Oops!',
+                subTitle: 'You do not have enough saving deposits to make this transaction',
+                buttons: [
+                  {
+                    text: 'Ok',
+                    role: 'cancel'
+                  }
+                ]
+              });
+              warning.present();
+              return true;
+            }
+
+            else {
+              this.withdrawal.amount = data.amount;
+              this.withdrawal.walletId = this.wallet._id;
+              let confirm = this.alertCtrl.create({
+                subTitle: 'Continue transaction?',
+                buttons: [
+                  {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    handler: () => {
+                    }
+                  },
+                  {
+                    text: 'Agree',
+                    handler: () => {
+                      this.presentLoad('Updating wallet...');
+                      this.transactionsProvider.addTransaction(this.withdrawal)
+                        .then((result) => {
+                          console.log(result);
+                          this.loading.dismiss();
+                          this.presentToast('Withdrawal Successful!');
+                          this.prompt.dismiss();
+                          this.getTransactions();
+                        }, err => {
+                          this.loading.dismiss();
+                          console.log(err);
+                          this.presentToast(err.message);
+                        });
+                    }
+                  }
+                ]
+              });
+              confirm.present();
+              return false;
+            }
+
+          }
+        }
+      ]
+    });
+    this.prompt.present();
+  }
+
+  close() {
     this.viewCtrl.dismiss();
   }
 
